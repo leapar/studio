774db21e5deafe6dd6e798d7981ad6add5de8e34
leapar <leapar@163.com>
2018-01-29 10:52:02 +0800

    edit

--- src/com/haulmont/studio/ui/h/b.java
+++ src/com/haulmont/studio/ui/h/b.java
@@ -2,6 +2,29 @@ package com.haulmont.studio.ui.h;
 
 import java.util.Set;
 
+
+/*
+        D:\javacodes\studio\lib\studio-ui-6.7.6.jar!\com\haulmont\studio\ui\h\a.class
+
+//接受UDP数据包  D:\javacodes\studio\lib\studio-ui-6.7.6.jar!\com\haulmont\studio\ui\h\b.class
+private void c() {
+        String var1 = this.c.d();
+        String var2 = this.c.f();
+        if (var2 != null) {
+        try {
+        this.f = new DatagramSocket(8732);
+        b var3 = new b(this, var1, var2);
+        this.e = Executors.newSingleThreadExecutor();
+        this.e.submit(var3);
+        } catch (SocketException var4) {
+        ;
+        }
+
+        }
+        }
+
+//如果序列号一致 机器码不同 时间戳小于当前时间戳 报错。
+*/
 class b implements Runnable {
     b(c var1, Set var2) {
     }
--- src/com/haulmont/studio/ui/h/d.java
+++ src/com/haulmont/studio/ui/h/d.java
@@ -2,6 +2,19 @@ package com.haulmont.studio.ui.h;
 
 import java.util.Set;
 
+/*
+* D:\javacodes\studio\lib\studio-ui-6.7.6.jar!\com\haulmont\studio\ui\h\c.class
+
+
+//启动udp包，发送本地序列信息 序列号^机器码^时间戳  D:\javacodes\studio\lib\studio-ui-6.7.6.jar!\com\haulmont\studio\ui\h\d.class
+private void d() {
+        Set var1 = this.e();
+        d var2 = new d(this, var1);
+        this.g = Executors.newSingleThreadScheduledExecutor();
+        this.g.scheduleAtFixedRate(var2, 0L, 60L, TimeUnit.SECONDS);
+        }
+
+*/
 class d implements Runnable {
     d(c var1, Set var2) {
     }
