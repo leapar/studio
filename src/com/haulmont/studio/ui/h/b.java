abffb17677aba5425226e42a0345d201de9bab51
leapar <leapar@163.com>
2018-01-29 10:27:46 +0800

    edit

---
+++ src/com/haulmont/studio/ui/h/b.java
@@ -0,0 +1,12 @@
+package com.haulmont.studio.ui.h;
+
+import java.util.Set;
+
+class b implements Runnable {
+    b(c var1, Set var2) {
+    }
+
+    public void run() {
+        return;
+    }
+}
---
+++ src/com/haulmont/studio/ui/h/d.java
@@ -0,0 +1,12 @@
+package com.haulmont.studio.ui.h;
+
+import java.util.Set;
+
+class d implements Runnable {
+    d(c var1, Set var2) {
+    }
+
+    public void run() {
+        return;
+    }
+}
